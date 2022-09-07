de5a441a8fc8663d357f6629961642fe11a62bec
RUFAI Ahmed <lamydas.ng@gmail.com>
2022-09-07 14:25:03 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Toast />
+</template>
+
+<script>
+import Toast from "./components/toast/Toast.vue";
+export default {
+  components: { Toast },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
