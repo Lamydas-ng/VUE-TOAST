ae64be9439f23ed0a16d9461b8485a4da5b551a9
RUFAI Ahmed <lamydas.ng@gmail.com>
2022-09-08 18:03:02 +0200

    Add positioning and transition styles to toast component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <Toast />
+
+<button @click="showToast = true">Show Toast</button>
+
+<Toast   
+    :show="showToast"
+    @hide="showToast = false" 
+    message="The message has been sent" 
+    type="warning"
+    position="bottom-left"
+    
+   
+    />
 </template>
 
 <script>
 import Toast from "./components/toast/Toast.vue";
 export default {
   components: { Toast },
+  data: ()=>({
+    showToast: false
+  }),
+
 };
 </script>
 
